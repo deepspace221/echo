a568346291f1c85f2e3d208d2c063f7962d4e807
deepspace <deepspace221@gmail.com>
2018-02-11 06:53:06 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -7,7 +7,7 @@ function getExpandableEmbed(title, color, thumb, description, fields, footerIcon
     if (color == "") color = randomColors;
     
     if (footer != undefined && footerIcon == undefined) footer = "{footer|icon:http://www.mahditajik.ir/wp-content/uploads/2016/07/com.memrise.android.memrisecompanion-1.png} \
-                                                    {footer|text:33"}";
+                                                                  {footer|text:33}";
     else if (footer != "" && footerIcon != "") footer = "{footer|icon:"+ footerIcon +"} \
                                                          {footer|text:dd"}";
     else footer = "";
