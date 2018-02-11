20c9fca844d17f12512b04fd0c5875b300380890
deepspace <deepspace221@gmail.com>
2018-02-11 06:33:10 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -27,7 +27,7 @@ function getExpendedEmbed(title, color, thumb, description, fields, footerIcon, 
 
 function getSimpleEmbed(title, thumb, description){
     var color = "#ff0000,#00ff00,#ffffff,#4286f4,#f45642,#262525,#e2d626,#87e226, #26e2c0,#2633e2,#8126e2";
-    if (thumb) thumb = "{guild|icon}"; else thumb = "https://i.stack.imgur.com/tfSdv.png";
+    if (thumb) thumb = "{guild|icon}"; else thumb = "https://images-ext-1.discordapp.net/external/Gzl0XG3nWwTNhcwHU69Onr9YmevH3wf7zVj1yN9PDE4/https/cdn.discordapp.com/icons/335021599059345408/55e56fb9d7850f3a1a9cfe8626c0a0d9.jpg?width=72&height=72";
 
     var embed = "{embed: \
                      {title:" + title +"} \
