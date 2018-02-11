2d76ac1366d43f48716ce120c45651c2bd1cb955
deepspace <deepspace221@gmail.com>
2018-02-11 07:26:35 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -1,26 +1,28 @@
 
 //Count
 function getExpandableEmbed(title, author, color, thumb, description, fields, footerIcon, footer)
+    var defaultFooterIcon = "http://www.mahditajik.ir/wp-content/uploads/2016/07/com.memrise.android.memrisecompanion-1.png";
+    var footer1 = "{footer|icon:" + defaultFooterIcon + "} {footer|text:" + footer +"}";
+    var footer2 = "{footer|icon:" + footerIcon + "} {footer|text:" + footer + "}";
+
     if (author) author = "{author|icon:{usericon}}{author|name:{self}}" else author = "";
     var randomColors = "#ff0000,#00ff00,#ffffff,#4286f4,#f45642,#262525,#e2d626,#87e226, #26e2c0,#2633e2,#8126e2";
-    if (thumb == "") thumb;
+    if (thumb == "") thumb = "" else thumb = "{thumb|url:"+ thumb +"}";
     if (fields == "") fields = "";
     if (color == "") color = randomColors;
     
-    if (footer != undefined && footerIcon == undefined) footer = "{footer|icon:http://www.mahditajik.ir/wp-content/uploads/2016/07/com.memrise.android.memrisecompanion-1.png} \
-                                                                  {footer|text:33}";
-    else if (footer != "" && footerIcon != "") footer = "{footer|icon:43} \
-                                                         {footer|text:dd}";
+    if (footer != ""){
+        if (footerIcon == "") footer = footer1;
+        if (footerIcon != "") footer = footer2;
+    }
     else footer = "";
-    
-    
-
+   
     var embed = "{embed: \
                      {title:" + title +"} \
                      {type: rich} \
                      " + author + " \
                      {color:{randlist:"+ color +"} \
-                    {thumb|url:"+ thumb +"} \
+                      " + thumb + " \
                     {desc: \
                " + description + " \
                       } \
