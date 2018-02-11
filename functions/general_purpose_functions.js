482b84baaf0f3722c27132b79249762438410863
deepspace <deepspace221@gmail.com>
2018-02-11 03:11:34 +0200

    Update general_purpose_functions.js

--- functions/general_purpose_functions.js
+++ functions/general_purpose_functions.js
@@ -1,1 +1,12 @@
 
+function debug(content){
+  webhook_url = A_VALID_DISCORD_WEBHOOK_URL
+  if(content === ""){
+    content = "Nothing passed into content" 
+  } else if(content.length > 1950 || (typeof content !== "string" && typeof content !== "number")){
+    content = "https://www.hastebin.com/" + JSON.parse(PostJSON("https://www.hastebin.com/documents", "", true, content)).key + ".js" 
+  } else {
+    content = "```js\n" + content + "\n```" 
+  }
+  PostJSON(webhook_url, "", false, {content: content})
+} 
