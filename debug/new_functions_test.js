558364290ada803aab7481012e96c43c870c1844
deepspace <deepspace221@gmail.com>
2018-02-12 00:51:20 +0200

    Update new_functions_test.js

--- debug/new_functions_test.js
+++ debug/new_functions_test.js
@@ -1,9 +1,21 @@
-function getMembersCount(){
+function getMembersCount(type){
 	var ctr = 0;
-	for (key in ServerMembers){
-		if (ServerMembers[key].Bot == false){
+	if (!type){
+		for (key in ServerMembers){
 			ctr++;
 		}
 	}	
+	else if (type == "users"){
+		for (key in ServerMembers){
+			if (!ServerMembers[key].User.Bot)
+				ctr++;
+		}
+	}
+	else if (type == "bots"){
+		for (key in ServerMembers){
+			if (ServerMembers[key].User.Bot)
+				ctr++;
+		}
+	}
 	return ctr;
 }
