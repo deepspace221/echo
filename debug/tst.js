81674ec9c0aabab63decc3cc7300b3e2d25c8289
deepspace <deepspace221@gmail.com>
2018-02-12 01:17:54 +0200

    Update tst.js

--- debug/tst.js
+++ debug/tst.js
@@ -1,9 +1,9 @@
-import "https://raw.githubusercontent.com/deepspace221/echo/master/functions/embeds.functions.js";
-import "https://raw.githubusercontent.com/deepspace221/echo/master/functions/general_purpose_functions.js";
+// import "https://raw.githubusercontent.com/deepspace221/echo/master/functions/embeds.functions.js";
+// import "https://raw.githubusercontent.com/deepspace221/echo/master/functions/general_purpose_functions.js";
 var color = GetJSON("https://raw.githubusercontent.com/deepspace221/echo/master/json/css-color-names.json");
 var redirect = "{redirect:383789043248267264}";
 var title = "Member Left";
 var description = "{user} has left the server!";
 var footer = "Current members count: " + getMembersCount("users");
 var color = color.red; 
-resp = redirect + getExpandableEmbed(title, true, color, "user", description, '' , '' , footer);
+var value = redirect + getExpandableEmbed(title, true, color, "user", description, '' , '' , footer);
