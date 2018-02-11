1e06087a5c0d2fb3ce4785abc49eb924616d198d
deepspace <deepspace221@gmail.com>
2018-02-11 23:52:15 +0200

    Update general_purpose_functions.js

--- functions/general_purpose_functions.js
+++ functions/general_purpose_functions.js
@@ -59,7 +59,7 @@ function getStaffRoleName(){ //working
 function getMembersCount(){
 	var ctr = 0;
 	for (i = 0; ServerMembers.length; i++){
-		if (ServerMembers[i].User.Bot == true)
+		if (!ServerMembers[i].User.Bot)
 			ctr++;
 	}	
 	return ctr;
