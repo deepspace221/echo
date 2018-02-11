c4b8eb94194114fd8e894b386c90873e81a26e83
deepspace <deepspace221@gmail.com>
2018-02-11 03:37:00 +0200

    Update general_purpose_functions.js

--- functions/general_purpose_functions.js
+++ functions/general_purpose_functions.js
@@ -1,4 +1,4 @@
-
+//DEBUG. Post json data to a webhook or hastebin if longer than 2000 chars.
 function debug(content){
   webhook_url = A_VALID_DISCORD_WEBHOOK_URL
   if(content === ""){
@@ -10,3 +10,39 @@ function debug(content){
   }
   PostJSON(webhook_url, "", false, {content: content})
 } 
+
+//GIVINIG ROLES. Sort the users roles and return them as an array;
+function sortRolesByPosition() {
+
+        var arr = new Array();
+
+        for (var i = 0; i < UserRoles.length; i++) {
+            for (var j = 0; j < ServerRoles.length; j++) {
+                if (UserRoles[i] === ServerRoles[j]["ID"]) {
+                    var role = {
+                        Position: ServerRoles[j]["Position"],
+                        Name: GetRoleName(UserRoles[i])
+                    };
+                    arr.push(role);
+                }
+            }
+        }
+
+        var byPos = arr.slice(0);
+
+        byPos.sort(function(a,b) {
+            return b.Position - a.Position;
+        });
+
+        return byPos;
+}
+
+//GIVINIG ROLES. Returns an array of restricted roles
+function getRestrictedRoles(){
+	var arr = [];
+	for (i=0; i < ServerRoles.length; i++){
+   		 if (/^[^\d]\s/.test(ServerRoles[i]["Name"]))
+     			 arr.push(ServerRoles[i]["Name"]);
+	}
+	return arr;
+}
