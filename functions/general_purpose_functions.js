a5277a14eec0c7aceb3227d3fe0f8f0ba7bbde75
deepspace <deepspace221@gmail.com>
2018-02-12 06:23:01 +0200

    Update general_purpose_functions.js

--- functions/general_purpose_functions.js
+++ functions/general_purpose_functions.js
@@ -94,7 +94,7 @@ function getRegexRoleName(roleName){
 function findUserImageAndUsernameByUserID(userID){
 	for (key in ServerMembers){
 	     if (ServerMembers[key].User.ID == userID){
-		     var userIcon = "https://discordapp.com/api/v6/users/" + userID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg"
+		     var userIcon = "https://discordapp.com/api/v6/users/" + userID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg";
 		     var userName = ServerMembers[key].User.Name;
 		     retrun {username: userName, avatar: userIcon};
 	     }	
