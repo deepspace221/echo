7cb0bd3bb6987b5044950bc1dc06f314de2ce864
deepspace <deepspace221@gmail.com>
2018-02-12 09:48:01 +0200

    Update new_functions_test.js

--- debug/new_functions_test.js
+++ debug/new_functions_test.js
@@ -1,11 +1,60 @@
-function findUserImageAndUsernameByUserID(userID){
-	for (key in ServerMembers){
-	     if (ServerMembers[key].User.ID == userID){
-		     var userIcon = "https://discordapp.com/api/v6/users/" + userID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg";
-		     var userName = ServerMembers[key].User.Username;
-		     var obj = {username: userName, avatar: userIcon};
-		     return obj;
-	     }	
+function updateMonthStats(type){
+	var date = new Date();
+	
+	var stats = {
+		joinMonthCounter: 0,
+                leftMonthCounter: 0,
+		joinDayCounter: 0,
+                leftDayCounter: 0,
+		sameDay: true,
+		sameMonth: true,
+		monthDay: date.getDate()
+	};
+
+	if (server_db["traffic"].date == undefined){
+	    server_db["traffic"].date = JSON.stringify(date);
+	    stats[type + "DayCounter"] = 1;
+        stats[type + "MonthCounter"] = 1;
+	    server_db["traffic"].stats = JSON.stringify(stats);
 	}
-        return false;
+	else {
+		dateDB = JSON.parse(server_db["traffic"].date)
+		stats = JSON.parse(server_db["traffic"].stats)
+		
+		if (date.getDate() == dateDB.getDate()){ 
+			stats[type + "DayCounter"]++;
+			stats[type + "MonthCounter"]++;
+			stats.sameDay = true;
+			stats.sameMonth = true;
+			server_db["traffic"].stats = JSON.stringify(stats);
+			return server_db["traffic"].dayCounter;
+		}
+		else if (date.getMonth() == dataDB.getMonth()){
+			stats[type + "DayCounter"] = 1;
+			stats[type + "MonthCounter"]++;
+			stats.sameDay = false;
+			stats.sameMonth = true;
+			server_db["traffic"].stats = JSON.stringify(stats);
+		}
+		else {
+			stats[type + "DayCounter"] = 1;
+			stats[type + "MonthCounter"] = 1;
+			stats.sameDay = false;
+			stats.sameMonth = false;
+			server_db["traffic"].stats = JSON.stringify(stats);			
+		}
+	}
+}
+
+function minutesUntilMidnight() {
+    var midnight = new Date();
+    midnight.setHours( 24 );
+    midnight.setMinutes( 0 );
+    midnight.setSeconds( 0 );
+    midnight.setMilliseconds( 0 );
+    return ( midnight.getTime() - new Date().getTime() ) / 1000 / 60;
+}
+
+function isSameDay(a, b) {
+    return a.toDateString() == b.toDateString();
 }
