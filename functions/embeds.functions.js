5de523849beeadbef387847c69d1f5646472a88b
deepspace <deepspace221@gmail.com>
2018-02-12 03:56:45 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -38,13 +38,16 @@ function getExpandableEmbed(title, author, color, thumb, description, fields, fo
 
 function getSimpleEmbed(title, thumb, description){
     var color = "{randlist:#ff0000,#00ff00,#ffffff,#4286f4,#f45642,#262525,#e2d626,#87e226,#26e2c0,#2633e2,#8126e2}";
-    if (!thumb || thumb == true) thumb = "{guild|icon}";
+    
+    if (!thumb || thumb == true) thumb = "{thumb|url:{guild|icon}}";
+    else if (thumb == "user") thumb = "{thumb|url:{usericon}}";
+    else thumb = "{thumb|url:"+ thumb +"}";
 
     var embed = "{embed: \
                      {title:" + title +"} \
                      {type: rich} \
                      {color:"+ color +"} \
-                    {thumb|url:"+ thumb +"} \
+                     "+ thumb +" \
                     {desc: \
                " + description + " \
                       } \
