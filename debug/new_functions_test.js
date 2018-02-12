73eb2ec933c65c1efa4187a9796161dc5cd11666
deepspace <deepspace221@gmail.com>
2018-02-12 06:45:20 +0200

    Update new_functions_test.js

--- debug/new_functions_test.js
+++ debug/new_functions_test.js
@@ -3,7 +3,8 @@ function findUserImageAndUsernameByUserID(userID){
 	     if (ServerMembers[key].User.ID == userID){
 		     var userIcon = "https://discordapp.com/api/v6/users/" + userID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg";
 		     var userName = ServerMembers[key].User.Name;
-		     retrun {username: userName, avatar: userIcon};
+		     var obj = {username: userName, avatar: userIcon};
+		     retrun obj;
 	     }	
 	}
 }
