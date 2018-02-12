f5156c4d9a200cb3f29d94f9a8d40e11a129163f
deepspace <deepspace221@gmail.com>
2018-02-12 05:48:02 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -11,11 +11,11 @@ function getExpandableEmbed(title, author, color, thumb, description, fields, fo
     if (author) author = "{author|icon:{usericon}}{author|name:{user}}"; else author = "";
     var randomColors = "{randlist:#ff0000,#00ff00,#ffffff,#4286f4,#f45642,#262525,#e2d626,#87e226,#26e2c0,#2633e2,#8126e2}";
     
-    if (!thumb || thumb === true) thumb = "{thumb|url:{guild|icon}}";
-    else if (thumb == "false") thumb = "";
-    else if (thumb == "user") thumb = "{thumb|url:{usericon}}";
-    else thumb = "{thumb|url:"+ thumb +"}";
-    
+    if (thumb == "user") thumb = "{thumb|url:{usericon}}";
+    else if (thumb == "server") thumb = "{thumb|url:{guild|icon}}";
+    else if (thumb) thumb = "{thumb|url:"+ thumb +"}";
+    else if (!thumb) thumb = "";
+   
     if (fields == "") fields = "";
     if (!color) color = randomColors;
     
@@ -43,10 +43,13 @@ function getExpandableEmbed(title, author, color, thumb, description, fields, fo
 function getSimpleEmbed(title, thumb, description){
     if (!color) color = "{randlist:#ff0000,#00ff00,#ffffff,#4286f4,#f45642,#262525,#e2d626,#87e226,#26e2c0,#2633e2,#8126e2}"; 
     
-    if (!thumb || thumb === true) thumb = "{thumb|url:{guild|icon}}";
-    else if (thumb === "false") thumb = "";
-    else if (thumb == "user") thumb = "{thumb|url:{usericon}}";
-    else thumb = "{thumb|url:"+ thumb +"}";
+   
+    if (thumb == "user") thumb = "{thumb|url:{usericon}}";
+    else if (thumb == "server") thumb = "{thumb|url:{guild|icon}}";
+    else if (thumb) thumb = "{thumb|url:"+ thumb +"}";
+    else if (!thumb) thumb = "";
+
+
 
     var embed = "{embed: \
                      {title:" + title +"} \
