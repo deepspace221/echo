e5c72a5e4c0f502672fe67cb5b5d1a16d824903d
deepspace <deepspace221@gmail.com>
2018-02-12 22:22:21 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -78,4 +78,109 @@ function getEmbed(type){
       else if (type == "monthly"){
          
       }
+      else if (type == "help"){
+         return getHelpEmbed();                        
+      }
+}
+
+function getHelpEmbed(){
+
+      var title = "Command examples for {guild|name}"; 
+
+      var description = " \
+      \nHello, {/user}! <:blank:352901517004636163> \
+      \n \
+      \nAccess to features in the this server is dependent upon the roles you have. \
+      \n \
+      \nPlease make sure to tag yourself properly so you'll have access to all the features this server has to offer. \
+      ";
+
+      var fields = " \
+      {field[0]|name:Select language <:blank:352901517004636163>} \
+      {field[0]|value: \
+      \n```css \
+      \n.iam n. english \
+      \n.iam f. spanish  \
+      \n.iam german  \
+      \n```} \
+      {field[0]|inline:true} \
+      {field[1]|name:Description <:blank:352901517004636163>} \
+      {field[1]|value: \
+      \n```fix \
+      \nFor native languages \
+      \nFor fluent languaages \
+      \nFor learning languages \
+      \n```} \
+      {field[1]|inline:true} \
+      {field[2]|name:Memrise <:blank:352901517004636163>} \
+      {field[2]|value: \
+      \n```css \
+      \n.iam memrise \
+      \n.iam memrise lvl 13 \
+      \n```} \
+      {field[2]|inline:true} \
+      {field[3]|name: <:blank:352901517004636163>} \
+      {field[3]|value: \
+      \n```fix \
+      \nFor Memrise users \
+      \nBetween 10-15 \
+      \n```} \
+      {field[3]|inline:true} \
+      {field[4]|name: Duolingo<:blank:352901517004636163>} \
+      {field[4]|value: \
+      \n```css \
+      \n.iam duolingo \
+      \n.iam 4 trees completed \
+      \n.iam 2 trees lvl 25 \
+      \n```} \
+      {field[4]|inline:true} \
+      {field[5]|name: <:blank:352901517004636163>} \
+      {field[5]|value: \
+      \n```fix \
+      \nFor Duolingo users \
+      \nBetween 1-5 \
+      \nBetween 1-3 \
+      \n```} \
+      {field[5]|inline:true} \
+      \n \
+      {field[6]|name:Misc <:blank:352901517004636163>} \
+      {field[6]|value: \
+      \n```css \
+      \n.iam polyglot \
+      \n.iam linguistics \
+      \n.iam translator \
+      \n.iam IPA \
+      \n.iam NSFW \
+      \n```} \
+      {field[6]|inline:true} \
+      \n \
+      {field[7]|name: <:blank:352901517004636163>} \
+      {field[7]|value: \
+      \n```fix \
+      \nFluent in 4 or above \
+      \nFor linguists \
+      \nFor translators \
+      \nFor IPA literate \
+      \nAge 18+ \
+      ```} \
+      {field[7]|inline:true} \
+      \n \
+      {field[8]|name:Removing a role <:blank:352901517004636163>} \
+      {field[8]|value: \
+      \n:point_right: To **remove** a role replace `.iam` with `.iamnot` \
+      \n} \
+      {field[8]|inline:false} \
+      \n \
+      {field[9]|name:Additional information} \
+      {field[9]|value: \
+      \n💠 Type: `\misc` for information on other roles on the server. \
+      \n💠 Type: `\lang` to view a full list of available languages. \
+      \n💠 Type: `\views` to view a list of aviliable view options. \
+      \n💠 Type `\platform` to view a full list of available platforms. \
+      \n💠 Type: `\cmd` to view a list of additional help commands. \
+      \n} \
+      {field[9]|inline:false} \
+      ";
+   
+      return  getExpandableEmbed(title, true, "", "server", description, fields, '', '');  
 }
