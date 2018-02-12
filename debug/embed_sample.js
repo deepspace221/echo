cb6b5cce4b1e4ec9966d422735f6864b6e66fedc
deepspace <deepspace221@gmail.com>
2018-02-13 00:36:51 +0200

    Update embed_sample.js

--- debug/embed_sample.js
+++ debug/embed_sample.js
@@ -1,19 +1,8 @@
-var value = {embed: \
-    {title:Command examples for {guild|name}}\
-    {type:rich}\
-    {author|name:{owner|name}}\
-    {author|icon:{owner|avatar}}\
-    {author|url:{owner|avatar}}\
-    {color:\
-        {randlist:\
-             #ff0000,#00ff00,#ffffff,#4286f4,\
-             #f45642,#262525,#e2d626,#87e226,\
-             #26e2c0,#2633e2,#8126e2\
-        }\
-    }\
-    {thumb|url:{guild|icon}}\
-    {desc:\
-{user} {/user} this is js mixed with an ars not in an imported file. \
-    }\
+var value = var value = " \
+{embed: \
+             {author}
+             {desc:\
+                {user} {/user} this is js mixed with an ars not in an imported file. \
+              }\
 }\
 ";
