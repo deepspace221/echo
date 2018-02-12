7bd09a1d7d6e2f8caf93cc6b27b2d1509a4e5448
deepspace <deepspace221@gmail.com>
2018-02-13 00:37:03 +0200

    Update embed_sample.js

--- debug/embed_sample.js
+++ debug/embed_sample.js
@@ -1,6 +1,5 @@
 var value = var value = " \
 {embed: \
-             {author}
              {desc:\
                 {user} {/user} this is js mixed with an ars not in an imported file. \
               }\
