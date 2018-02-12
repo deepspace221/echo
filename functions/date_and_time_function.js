db2389027e1600088e6eb06bf2a616f9d9cadee2
deepspace <deepspace221@gmail.com>
2018-02-12 09:47:01 +0200

    Update date_and_time_function.js

--- functions/date_and_time_function.js
+++ functions/date_and_time_function.js
@@ -1,3 +1,60 @@
+function updateMonthStats(type){
+	var date = new Date();
+	
+	var stats = {
+		joinMonthCounter: 0,
+        leftMonthCounter: 0,
+		joinDayCounter: 0,
+        leftDayCounter: 0,
+		sameDay: true,
+		sameMonth: true,
+		monthDay: date.getDate()
+	};
+
+	if (server_db["traffic"].date == undefined){
+	    server_db["traffic"].date = JSON.stringify(date);
+	    stats[type + "DayCounter"] = 1;
+        stats[type + "MonthCounter"] = 1;
+	    server_db["traffic"].stats = JSON.stringify(stats);
+	}
+	else {
+		dateDB = JSON.parse(server_db["traffic"].date)
+		stats = JSON.parse(server_db["traffic"].stats)
+		
+		if (date.getDate() == dateDB.getDate()){ 
+			stats[type + "DayCounter"]++;
+			stats[type + "MonthCounter"]++;
+			stats.sameDay = true;
+			stats.sameMonth = true;
+			server_db["traffic"].stats = JSON.stringify(stats);
+			return server_db["traffic"].dayCounter;
+		}
+		else if (date.getMonth() == dataDB.getMonth()){
+			stats[type + "DayCounter"] = 1;
+			stats[type + "MonthCounter"]++;
+			stats.sameDay = false;
+			stats.sameMonth = true;
+			server_db["traffic"].stats = JSON.stringify(stats);
+		}
+		else {
+			stats[type + "DayCounter"] = 1;
+			stats[type + "MonthCounter"] = 1;
+			stats.sameDay = false;
+			stats.sameMonth = false;
+			server_db["traffic"].stats = JSON.stringify(stats);			
+		}
+	}
+}
+
+function minutesUntilMidnight() {
+    var midnight = new Date();
+    midnight.setHours( 24 );
+    midnight.setMinutes( 0 );
+    midnight.setSeconds( 0 );
+    midnight.setMilliseconds( 0 );
+    return ( midnight.getTime() - new Date().getTime() ) / 1000 / 60;
+}
+
 function isSameDay(a, b) {
     return a.toDateString() == b.toDateString();
 }
