7577b7d6b168653283db8444b9eac99606989359
deepspace <deepspace221@gmail.com>
2018-02-12 10:07:03 +0200

    Update new_functions_test2.js

--- debug/new_functions_test2.js
+++ debug/new_functions_test2.js
@@ -2,10 +2,12 @@ function dbg(content){
    webhook_url = "https://discordapp.com/api/webhooks/412072411333263360/xdl1PAHXJ3rQowr0VwRG1a5EnavbyoKoZjeOZ4TGE71IEVjfBNFJO1HBh_RSTILT553Q";
    if(content === ""){
      content = "Nothing passed into content" 
-   } else if(content.length > 1950 || (typeof content !== "string" && typeof content !== "number")){
+   } 
+//    else if(content.length > 1950 || (typeof content !== "string" && typeof content !== "number")){
 //      content = "https://www.hastebin.com/" + JSON.parse(PostJSON("https://www.hastebin.com/documents", "", true, content)).key + ".js" 
-//    } else {
+// } 
+   else {
 //      content = "```js\n" + content + "\n```" 
-//    }
+// }
    PostJSON(webhook_url, "", false, {content: content})
 } 
