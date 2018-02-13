4b9a09cdf7af34d096bdcd54b762139e6a1bf0c4
deepspace <deepspace221@gmail.com>
2018-02-13 07:33:49 +0200

    Update new_functions_test.js

--- debug/new_functions_test.js
+++ debug/new_functions_test.js
@@ -2,7 +2,7 @@
 function getUserOBJ(user){
 	var regexp = new RegExp(user, 'i');
 	for (key in ServerMembers){
-	     if (ServerMembers[key].User.ID == userID || /regexp/.test(ServerMembers[key].User.Username)){
+	     if (ServerMembers[key].User.ID == user || /regexp/.test(ServerMembers[key].User.Username)){
 		     var obj = {};
 		     obj.userImage = "https://discordapp.com/api/v6/users/" + ServerMembers[key].User.ID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg";
 		     
