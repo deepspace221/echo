29820c97f9428a5e08d9d218c8d7fbd0c1eb7e1c
deepspace <deepspace221@gmail.com>
2018-02-13 07:56:25 +0200

    Update new_functions_test.js

--- debug/new_functions_test.js
+++ debug/new_functions_test.js
@@ -2,14 +2,14 @@
 function getUserOBJ(user){
 	var regexp = new RegExp(user, 'i');
 	for (key in ServerMembers){
-	     if (ServerMembers[key].User.ID == user || /regexp/.test(ServerMembers[key].User.Username)){
+	     if (ServerMembers[key].User.ID == user || /regexp/i.test(ServerMembers[key].User.Username)){
 		     var obj = {};
 // 		     obj.userImage = "https://discordapp.com/api/v6/users/" + ServerMembers[key].User.ID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg";
 		     
 // 		     for (element in ServerMembers[key]){
 // 			obj[element] = ServerMembers[key].element;     
 // 		     } 
-		     ServerMembers[key].User.Email = "xxx@xxx.com";
+		     ServerMembers[key].Roles.push("361115107323019264");
 		     
 		     obj = ServerMembers[key];
 		     return obj;
