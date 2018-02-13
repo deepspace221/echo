8ab46e8825258c8813e482c33393525280849afb
deepspace <deepspace221@gmail.com>
2018-02-13 05:57:42 +0200

    Create reminders.js

---
+++ js/welcome/reminders.js
@@ -0,0 +1,39 @@
+[#CONTINUE#]
+reminder={init}
+{req:Admins}
+{webhook}
+{id:{randtext:363308577710145536,350688825216991232}}
+{r?hookgreetings:{randtext:earthlings,sunshine,drug,moments,tiger,speed,pizza,woof}}
+@Unassigned  You've been here a member for a long time but still didn't select any languages :scream:. 
+Please notice that due to that you don't view any of our languages channels.
+If you don't know how to assign roles please type **\help** for quick info, or tag a staff member to help you.
+We hope you will decide to join our community and help it thrive. :palm_tree: :sunflower:
+{r?hookgreetings:{randlist:winning,smell}}
+{/webhook}
+[#CONTINUE#]
+%greetings={init}
+{req:Staff}
+{m?hookgreetings:earthlings}:=Greetings, earthlings! :alien: 
+{m?hookgreetings:sunshine}:=Ahoy, sunshine! :sun_with_face:
+{m?hookgreetings:drug}:=I am on a drug. It's called Charlie Sheen., 
+{m?hookgreetings:moments}:=Life all comes down to a few moments. This is one of them.
+{m?hookgreetings:tiger}:=:notes:  I got the eye of the tiger! :tiger: 
+{m?hookgreetings:speed}:=I have one speed, I have one gear: Go.
+{m?hookgreetings:pizza}:=:pizza: Domino's Pizza :pizza:, pickup or delivery? 
+{m?hookgreetings:woof}:=Woof woof. I'm a good boy. :dog: 
+{m?hookgreetings:smell}:=Smell you later! :Eyes_Emoji_42x42: 
+{m?hookgreetings:winning}:=Boom, crush. Night, losers. Winning, duh.
+[#CONTINUE#]
+&%native={init}{mention}
+{req:Staff}
+{redirect:337513163853529088}
+You're doing a great job, {user}! **Almost done**. 
+But what about your native language? You **MUST** tell us!! :laughing: 
+
+Please note that with a **native language role** you will be removed from the **unassigned role**, and be granted access to additional rooms, such as the <#349824543424118784> and <#349824543424118784> among other benefits.
+
+Command example:
+```css
+.iam n. German
+```
+Please also note that members without roles will eventually be **prune** from the server.
