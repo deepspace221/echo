bfb6aaa36f59fc0c1b629904536916f3b82540df
deepspace <deepspace221@gmail.com>
2018-02-13 06:47:20 +0200

    Create small_help_snippets.js

---
+++ help/small_help_snippets.js
@@ -0,0 +1,7 @@
+&\greet={init}
+{req:Staff,Ambassadors}
+
+**<@{/rawid}> welcomes you to the server, {user} !** :happy_panda:
+What are your native/fluent/learning languages? We need this information in order to tag you.
+Run **\help** to view instructions how to do it by yourself. 
+If you're unsure what to do please just write the information in plain English and a staff member will assist you.
