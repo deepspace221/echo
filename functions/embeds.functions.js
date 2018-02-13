4d8df57647795e8e5cac68c2bbaf718833f5324f
deepspace <deepspace221@gmail.com>
2018-02-13 02:24:31 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -48,7 +48,7 @@ function getExpandableEmbed(title, author, color, thumb, description, fields, fo
     return embed;
 }
 
-function getSimpleEmbed(title, thumb, description){
+function getSimpleEmbed(title, thumb, description, color){
     if (!color) color = "{randlist:#ff0000,#00ff00,#ffffff,#4286f4,#f45642,#262525,#e2d626,#87e226,#26e2c0,#2633e2,#8126e2}"; 
     
    
