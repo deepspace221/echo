08bb3bda79e1f00a3ca1132b3b7b851833dfadb6
deepspace <deepspace221@gmail.com>
2018-02-13 07:13:47 +0200

    Update and rename sample.js to sample_new_functions.js

--- debug/sample.js
+++
@@ -1,7 +0,0 @@
-%auto tst={init}
-#js >>
-import "https://raw.githubusercontent.com/deepspace221/echo/master/debug/new_functions_test.js"
-var result = ""
-
-resp = result;
->>
---
+++ debug/sample_new_functions.js
@@ -0,0 +1,7 @@
+%auto tst={init}
+#js >>
+import "https://raw.githubusercontent.com/deepspace221/echo/master/debug/new_functions_test.js";
+var result = "";
+
+resp = result;
+>>
