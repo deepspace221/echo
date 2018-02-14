220f4a6f4c8100e4955dc14bb03b7a7985152516
deepspace <deepspace221@gmail.com>
2018-02-14 20:50:21 +0200

    Create debug.functions.js

---
+++ functions/debug.functions.js
@@ -0,0 +1,17 @@
+function dbg(content){
+   webhook_url = "https://discordapp.com/api/webhooks/412072411333263360/xdl1PAHXJ3rQowr0VwRG1a5EnavbyoKoZjeOZ4TGE71IEVjfBNFJO1HBh_RSTILT553Q";
+   if(content === ""){
+     content = "Nothing passed into content" 
+   } 
+   else if (typeof content == "object"){
+      content = JSON.stringify(content);  
+   }
+   
+   if(content.length > 1950){
+     content = "https://www.hastebin.com/" + JSON.parse(PostJSON("https://www.hastebin.com/documents", "", true, content)).key + ".js" 
+   } 
+   else {
+     content = "```js\n" + content + "\n```" 
+   }
+   PostJSON(webhook_url, "", false, {content: content})
+} 
