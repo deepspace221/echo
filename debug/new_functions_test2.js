ef9eae070d21ed61e2b3853149c77c9c85d5c843
deepspace <deepspace221@gmail.com>
2018-02-14 20:20:53 +0200

    Update new_functions_test2.js

--- debug/new_functions_test2.js
+++ debug/new_functions_test2.js
@@ -3,11 +3,11 @@ function dbg(content){
    if(content === ""){
      content = "Nothing passed into content" 
    } 
-//    else if(content.length > 1950 || (typeof content !== "string" && typeof content !== "number")){
-//      content = "https://www.hastebin.com/" + JSON.parse(PostJSON("https://www.hastebin.com/documents", "", true, content)).key + ".js" 
-// } 
+   else if(content.length > 1950 || (typeof content !== "string" && typeof content !== "number")){
+     content = "https://www.hastebin.com/" + JSON.parse(PostJSON("https://www.hastebin.com/documents", "", true, content)).key + ".js" 
+} 
    else {
-//      content = "```js\n" + content + "\n```" 
-// }
+     content = "```js\n" + content + "\n```" 
+}
    PostJSON(webhook_url, "", false, {content: content})
 } 
