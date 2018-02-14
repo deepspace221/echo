e74876a5d212038380b48827915e9cac13c8b842
deepspace <deepspace221@gmail.com>
2018-02-14 05:28:54 +0200

    Update general_purpose_functions.js

--- functions/general_purpose_functions.js
+++ functions/general_purpose_functions.js
@@ -110,7 +110,7 @@ function getUserOBJ(user){
 		     var obj = {};   
           	     obj = JSON.parse(JSON.stringify(ServerMembers[key]));
                      obj.userImage = "https://discordapp.com/api/v6/users/" + ServerMembers[key].User.ID + "/avatars/" + ServerMembers[key].User.Avatar + ".jpg";
-		     return ServerMembers[key];
+		     return obj;
 	     }	
 	}
         return false;
