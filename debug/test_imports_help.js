a33843042e5b7b08c8f13a9890fd0800845cc300
deepspace <deepspace221@gmail.com>
2018-02-14 08:30:31 +0200

    Create test_imports_help.js

---
+++ debug/test_imports_help.js
@@ -0,0 +1,9 @@
+%auto &test {params}={init}
+#js >>
+import "https://raw.githubusercontent.com/deepspace221/echo/master/functions/embeds.functions.js";
+import "https://raw.githubusercontent.com/deepspace221/echo/master/help/welcome_help_messages_functions.js";
+import "https://raw.githubusercontent.com/deepspace221/echo/master/functions/general_purpose_functions.js";
+import "https://raw.githubusercontent.com/deepspace221/echo/master/help/help_bots_playground.js";
+
+resp = getEmbed(Params) ;
+>>
