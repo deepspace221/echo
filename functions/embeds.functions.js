d236f5425dc0a0e4fea089dfecfccbdbae15eda3
deepspace <deepspace221@gmail.com>
2018-02-14 21:35:46 +0200

    Update embeds.functions.js

--- functions/embeds.functions.js
+++ functions/embeds.functions.js
@@ -130,3 +130,24 @@ function getEmbed(type){
     };
 }
 
+function getEmbed(){
+    var embed = " \
+        {embed: \
+                 {title:" + title +"} \
+                 {type: rich} \
+                 {color: \
+                        {randlist: \
+                                 #ff0000,#00ff00,#ffffff,#4286f4, \
+                                 #f45642,#262525,#e2d626,#87e226, \
+                                 #26e2c0,#2633e2,#8126e2 \
+                       } \
+                {thumb|url:"+ thumb +"} \
+                {desc: \
+           " + description + " \
+                  } \
+            " + fields + " \
+         } \
+    ";
+    return embed;
+}
+
