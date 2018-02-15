73db909fe3a6221cb8d5056bc678b8738b3e7e06
deepspace <deepspace221@gmail.com>
2018-02-15 02:35:48 +0200

    Update count.echo.functions.js

--- functions/count.echo.functions.js
+++ functions/count.echo.functions.js
@@ -43,7 +43,8 @@ function getLangsInfoOBJ(arrStudyLangs, fluentIndex, nativeIndex){
             outputOBJ.fluentRolesCount = langsSliceOBJ.arrFluent.length;
             outputOBJ.learningRolesCount = langsSliceOBJ.arrLearning.length;
             
-  
+             dbg(outputOBJ);
+    
               
         
             // function compare(a, b) {
@@ -71,7 +72,7 @@ function getLangsInfoOBJ(arrStudyLangs, fluentIndex, nativeIndex){
                         outputOBJ[typeLC + "Num"] += "{membercount:" + langsSliceOBJ["arr" + type][length - i].name + "}\n";
                     }
             }
-            dbg(outputOBJ);
+            dbg(produceOutput);
             // for (i=0; i < arrStudyLangs.length; i++){
             //     if (i < fluentIndex) {
             //         outputOBJ.learningField += arrStudyLangs[i].name + ": " + "**{membercount:" + arrStudyLangs[i].name + "}**\n";
