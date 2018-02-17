115b439c1746f48e0328a2649467304e70b569fd
deepspace <deepspace221@gmail.com>
2018-02-17 03:38:54 +0200

    Create count_embed_functions.js

---
+++ functions/messages/embeds/count_embed_functions.js
@@ -0,0 +1,13 @@
+function getSingleRoleCount(roleName, roleCount){
+        var fields = "";
+        var footer = "";
+        var thumb = "";
+        var footerIcon ="";
+        var color = "";
+        var author= "";
+        title = "The number of "+ roleName + " users:";
+        description = " \
+                \nThe total number of users in role " + roleName +" is: " + roleCount +" \
+                ";              
+        resp = getExpandableEmbed(title, author, color, thumb, description, fields, footerIcon, footer);
+}
