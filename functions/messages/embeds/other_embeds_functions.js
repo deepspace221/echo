549f053137b65045c839b4322cc39a33eda3ad50
deepspace <deepspace221@gmail.com>
2018-02-18 08:58:24 +0200

    Create other_embeds_functions.js

---
+++ functions/messages/embeds/other_embeds_functions.js
@@ -0,0 +1,11 @@
+function getRolesEmbed(outputMsg){
+      var title = "Role Assignment";
+      var color = "";
+      var thumb = "";
+      var description = "outputMsg";
+      var footIcon = "";
+      var footer = "";
+      var fields = "";
+    
+      return  getExpandableEmbed(title, "", color, thumb, description, fields, "", "");  
+}
