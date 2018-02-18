e408b236916ccb68151e604246b5c8beaf396dea
deepspace <deepspace221@gmail.com>
2018-02-18 09:29:00 +0200

    Update other_embeds_functions.js

--- functions/messages/embeds/other_embeds_functions.js
+++ functions/messages/embeds/other_embeds_functions.js
@@ -1,4 +1,19 @@
-function getRolesEmbed(outputMsg){
+function getRolesEmbed(roles){
+      var roleList = "";
+      var takeList = ""; 
+      var polyglot = "false";
+      var errMsg = ""
+        
+	for (var i = 0; i < roles.giveMsg.length; i++){
+	       roleList += "`" + roles.giveMsg[i] + "` ";
+	}
+	for (var i = 0; i < roles.takeMsg.length; i++){
+	       takeList += "`" + roles.takeMsg[i] + "` ";
+	}
+	for (var i = 0; i < roles.errMsg.length; i++){
+	       errMsg += "`" + roles.errMsg[i] + "` ";
+	}
+  
       var title = "ROLES TOGGLE INFO";
       var color = "";
       var thumb = "";
@@ -6,6 +21,22 @@ function getRolesEmbed(outputMsg){
       var footIcon = "";
       var footer = "";
       var fields = "";
-    
-      return  getExpandableEmbed(title, "", color, thumb, description, fields, "", "");  
+      
+    var fields = " \
+          {field[0]|name:Give} \
+          {field[0]|value:"+ roleList + "}  \
+          {field[0]|inline:true} \
+          {field[1]|name:take} \
+          {field[1]|value:" + takeList + "} \
+          {field[1]|inline:true} \
+          {field[2]|name:Polyglot} \
+          {field[2]|value:" + polyglot + "} \
+          {field[2]|inline:true} \
+          {field[3]|name:Errors} \
+          {field[3]|value:" + errMsg + "} \
+          {field[3]|inline:false} \
+
+          ";
+
+      return  getExpandableEmbed(title, "", color, thumb, description, fields, "", "");     
 }
