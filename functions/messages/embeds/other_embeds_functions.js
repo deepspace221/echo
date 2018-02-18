65dd82e40acb8ca1faa8af1a163b437b1f78de41
deepspace <deepspace221@gmail.com>
2018-02-18 09:15:03 +0200

    Update other_embeds_functions.js

--- functions/messages/embeds/other_embeds_functions.js
+++ functions/messages/embeds/other_embeds_functions.js
@@ -2,7 +2,7 @@ function getRolesEmbed(outputMsg){
       var title = "Role Assignment";
       var color = "";
       var thumb = "";
-      var description = "outputMsg";
+      var description = outputMsg;
       var footIcon = "";
       var footer = "";
       var fields = "";
