f09070559a1de95144cd0dccf723421b2b045f60
deepspace <deepspace221@gmail.com>
2018-02-18 07:21:01 +0200

    Update utilities_functions.js

--- functions/utilities_functions.js
+++ functions/utilities_functions.js
@@ -13,13 +13,13 @@ function findShortestStringInArr(arr){
 // }
 
 function escapeRegExp(str) {
-	var arr = ["\\-","\\[","\\]","\\/","\\{","\\}","\\(","\\)","\\*","\\+","\\?","\\.","\\\\","\\^","\\$","\\|"];
-// 	var arr = ["\-","\[","\]","\/","\{","\}","\(","\)","\*","\+","\?","\.","\\","\^","\$","\|"];
-	for (var i = 0; i < arr.length; i++){
-		var regExp = new RegExp(arr[i], "g")
-		str.replace(regExp, arr[i]);
-	}
-	dbg(str);
+// 	var arr = ["\\-","\\[","\\]","\\/","\\{","\\}","\\(","\\)","\\*","\\+","\\?","\\.","\\\\","\\^","\\$","\\|"];
+// // 	var arr = ["\-","\[","\]","\/","\{","\}","\(","\)","\*","\+","\?","\.","\\","\^","\$","\|"];
+// 	for (var i = 0; i < arr.length; i++){
+// 		var regExp = new RegExp(arr[i], "g")
+// 		str.replace(regExp, arr[i]);
+// 	}
+// 	dbg(str);
 	return str;
 }
 
