6f3a9a7581efcaa0d5b12f4974e18cd70201d670
deepspace <deepspace221@gmail.com>
2018-02-18 09:59:04 +0200

    Update roles.functions.js

--- functions/roles.functions.js
+++ functions/roles.functions.js
@@ -146,7 +146,7 @@ function getRolesOutput(roles){
 	}
 
 	if (roles.take.length == 2){
-                take = "\n{m?roles_db:takequite}:={take}" + roles.take.shift() + ", " + roles.take.shift() + ", Basic{/take}";
+                take = "\n{m?roles_db:takequite}:={take}Basic, " + roles.take.shift() + ", " + roles.take.shift() + "{/take}";
 	}
 	else if (roles.take.length == 1){
 	        take = "{take:" + roles.take.shift() + "}";
