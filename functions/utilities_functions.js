189aa7f42ac7570478b4aca42dd025698f00b7bd
deepspace <deepspace221@gmail.com>
2018-02-19 20:56:34 +0200

    Update utilities_functions.js

--- functions/utilities_functions.js
+++ functions/utilities_functions.js
@@ -23,3 +23,14 @@ function escapeRegExp(str) {
 	return str;
 }
 
+function createArrOutputCommaSeprated(arr){
+	var output = "";
+	for (var i = 0; i < arr.length; i++){
+		if (i == (arr.length - 1)){
+			output += arr[i];
+			break;
+		}		
+	       output += arr[i] + ", ";
+	}
+	return output;
+}
