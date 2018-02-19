1bd8e3f4fb212b2d4edc237c5042bb9c4a269490
deepspace <deepspace221@gmail.com>
2018-02-19 19:37:22 +0200

    Update roles.functions.js

--- functions/roles.functions.js
+++ functions/roles.functions.js
@@ -275,9 +275,10 @@ function verifyUserHasNativeAndChkPolyglot(roles){
 	
 	var polyglot = nativeFluentCtr.native + nativeFluentCtr.fluent;
 	if (polyglot >= 4){
-		dbg("native: " nativeFluentCtr.native + " flunet: " + nativeFluentCtr.fluent);
+// 		dbg("native: " nativeFluentCtr.native + " flunet: " + nativeFluentCtr.fluent);
 		roles.polyglot = true;
 		roles.give.push("Polyglot");
 		roles.notesMsg.push("\nPolyglot role has been assigned, since you're native or fluent in at least 4 languages.");
+		
 	}
 }
