29c0fc6be1ad225a9c741f9d5b522c255c6b1855
deepspace <deepspace221@gmail.com>
2018-02-21 07:39:01 +0200

    Update roles.functions.js

--- functions/roles.functions.js
+++ functions/roles.functions.js
@@ -279,7 +279,7 @@ function verifyUserHasNativeAndChkPolyglot(roles){
 		for (var i = 0; i < roles.take.length && limit > 0; i++){
 			if (/n\.\s/.test(roles.take[i])) {
 				limit--;
-				deb(roles.take[i]);
+				dbg(roles.take[i]);
 				roles.errMsg.push("Unable to preform role removel **" + roles.take[i] + "** - user only native language ERR.");
 				roles.takeMsg = "";
 				roles.take[i] = "Deleted";
