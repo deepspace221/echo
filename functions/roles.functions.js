045c9dd188209dae0f474a6edd0666bfd37d27e5
deepspace <deepspace221@gmail.com>
2018-02-21 02:50:54 +0200

    Update roles.functions.js

--- functions/roles.functions.js
+++ functions/roles.functions.js
@@ -353,10 +353,10 @@ function getRolesFieldOutputSplitted(arr){
 			obj.roleOther.push(arr[i]);
 	}	
 		
-	if (obj.roleNative.length != 0) output += "   <Native> " + createArrOutputCommaSeprated(obj.roleNative) + "\n";
-	if (obj.roleFluent.length != 0) output += "   <Fluent> " + createArrOutputCommaSeprated(obj.roleFluent) + "\n";
+	if (obj.roleNative.length != 0) output += " <Native> " + createArrOutputCommaSeprated(obj.roleNative) + "\n";
+	if (obj.roleFluent.length != 0) output += " <Fluent> " + createArrOutputCommaSeprated(obj.roleFluent) + "\n";
 	if (obj.roleLearning.length != 0) output += "<Learning> " + createArrOutputCommaSeprated(obj.roleLearning) + "\n";
-	if (obj.roleOther.length != 0) output += "   <Other> " + createArrOutputCommaSeprated(obj.roleOther) + "\n";
+	if (obj.roleOther.length != 0) output += "  <Other> " + createArrOutputCommaSeprated(obj.roleOther) + "\n";
 	if (output.length == 6) output += "<NaN>";
 
 	output += "```";
