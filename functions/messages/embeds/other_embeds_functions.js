5c418f397d54a2f7df245ba4b1c530f87c4796d0
deepspace <deepspace221@gmail.com>
2018-02-21 09:30:57 +0200

    Update other_embeds_functions.js

--- functions/messages/embeds/other_embeds_functions.js
+++ functions/messages/embeds/other_embeds_functions.js
@@ -1,9 +1,9 @@
 function getBasicRoleEmbed(roles){
 	
-	var description = "";
-	var color = "#00ee0b";
-	if (roles.lastElement.type == "give") description = "<@" + UserID + "> I have give you the role **" + roles.lastElement.role + "**.";
-	if (roles.lastElement.type == "take") description = "<@" + UserID + "> I have take from you the role **" + roles.lastElement.role + "**.";
+	var description = "", color = "";
+	
+	if (roles.lastElement.type == "give") {description = "<@" + UserID + "> I have give you the role **" + roles.lastElement.role + "**."; color = "#00ee0b";}
+	if (roles.lastElement.type == "take") {description = "<@" + UserID + "> I have take from you the role **" + roles.lastElement.role + "**."; color = "#f0e102";}
 	if (roles.errMsgStr != "") {description = "<@" + UserID + "> " + roles.errMsgStr; color = "#f70508"};
 	
 	return getBasicEmbed(color, description);
