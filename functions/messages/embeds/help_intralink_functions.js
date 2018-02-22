cd6d19627b5c7673c249a1301e17973f38a2d14d
deepspace <deepspace221@gmail.com>
2018-02-22 11:21:24 +0200

    Create help_intralink_functions.js

---
+++ functions/messages/embeds/help_intralink_functions.js
@@ -0,0 +1,31 @@
+function getIntralinkHelp(){
+    var title = "Commands help for ars://memriseandduolingo intralink server. ";
+    var author = "";
+    var color = "#dcf608";
+    // var thumb = "https://images-ext-1.discordapp.net/external/Gzl0XG3nWwTNhcwHU69Onr9YmevH3wf7zVj1yN9PDE4/https/cdn.discordapp.com/icons/335021599059345408/55e56fb9d7850f3a1a9cfe8626c0a0d9.jpg"; 
+    var thumb = ""; 
+    var description = " \
+    \n`\\lang euronews` \
+    \n`\\lang easy languages` \
+    \n`\\lang platforms` \
+    \n`\\lang dictionaries` \
+    \n`\\lang tools` \
+    \n`\\lang games` \
+    \n`\\lang tv` \
+    \n`\\lang info` \
+    \n`\\lang speaking` \
+    \n`\\lang duolingo` \
+    \n`\\lang memrise` \
+    \n`\\add [category] [name] [link]` \
+    \n`\\remove [category] [search_term]` \
+    ";
+    var fields;
+    var footericon ="http://androidcenter.com/wp-content/uploads/MEMRISE.png";
+    var footer = "Info provided by the Memrise & Duolingo language server";
+
+  return  getExpandableEmbed(title, author, color, thumb, description, fields, footerIcon, footer);  
+  
+  
+}
+  
+  
