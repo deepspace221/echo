d79343a37fc78a351c3222b897fff77676250699
deepspace <deepspace221@gmail.com>
2018-02-22 11:25:00 +0200

    Update help_intralink_functions.js

--- functions/messages/embeds/help_intralink_functions.js
+++ functions/messages/embeds/help_intralink_functions.js
@@ -1,9 +1,13 @@
 function getIntralinkHelp(){
+        // var thumb = "https://images-ext-1.discordapp.net/external/Gzl0XG3nWwTNhcwHU69Onr9YmevH3wf7zVj1yN9PDE4/https/cdn.discordapp.com/icons/335021599059345408/55e56fb9d7850f3a1a9cfe8626c0a0d9.jpg"; 
+
     var title = "Commands help for ars://memriseandduolingo intralink server. ";
-    var author = "";
     var color = "#dcf608";
-    // var thumb = "https://images-ext-1.discordapp.net/external/Gzl0XG3nWwTNhcwHU69Onr9YmevH3wf7zVj1yN9PDE4/https/cdn.discordapp.com/icons/335021599059345408/55e56fb9d7850f3a1a9cfe8626c0a0d9.jpg"; 
+    var author = "";
     var thumb = ""; 
+    var footerIcon ="http://androidcenter.com/wp-content/uploads/MEMRISE.png";
+    var footer = "Info provided by the Memrise & Duolingo language server";
+    
     var description = " \
     \n`\\lang euronews` \
     \n`\\lang easy languages` \
@@ -19,13 +23,9 @@ function getIntralinkHelp(){
     \n`\\add [category] [name] [link]` \
     \n`\\remove [category] [search_term]` \
     ";
+    
     var fields;
-    var footericon ="http://androidcenter.com/wp-content/uploads/MEMRISE.png";
-    var footer = "Info provided by the Memrise & Duolingo language server";
-
-  return  getExpandableEmbed(title, author, color, thumb, description, fields, footerIcon, footer);  
-  
-  
+  return getExpandableEmbed(title, author, color, thumb, description, fields, footerIcon, footer);   
 }
   
   
