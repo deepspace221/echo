0ae9feddce861ff4ffe9affd76d595f1569fc412
deepspace <deepspace221@gmail.com>
2018-02-23 12:03:48 +0200

    Update init_db_values.js

--- functions/init_db_values.js
+++ functions/init_db_values.js
@@ -102,6 +102,7 @@ function getRolesSlices(arrInput, startIndex, type){
                  }
                          
         }
+        dbg(JSON.stringify(arrOutput));
         return arrOutput;
 
         function findFirstIndexOfFluentOrLearning(){
