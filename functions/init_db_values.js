74fa781cbfe40ac91c9c0e916a490f18fe920f5d
deepspace <deepspace221@gmail.com>
2018-02-24 06:04:11 +0200

    Update init_db_values.js

--- functions/init_db_values.js
+++ functions/init_db_values.js
@@ -197,7 +197,7 @@ function storeServerRolesSlices(initValues){
 //            rolesSlicesObj.duolingo = getRolesSlices(arrSortedServerRolesObj, getNewPos(startPosObj.duolingoTopRoleIndex), "duolingo");
 //            rolesSlicesObj.memrise = getRolesSlices(arrSortedServerRolesObj, getNewPos(startPosObj.memriseTopRoleIndex), "color");
 //            rolesSlicesObj.views = getRolesSlices(arrSortedServerRolesObj, getNewPos(startPosObj.viewsTopRoleIndex), "color");
-           rolesSlicesObj.permissions = getRolesSlices(arrSortedServerRolesObj, 0, "permissions");
+           rolesSlicesObj.permissions = getRolesSlices(arrSortedServerRolesObj, 1, "permissions");
         
         dbg(rolesSlicesObj.permissions);
         
