55fc7cda9bdf0f5573e81492b80b37e5fc17d81d
deepspace <deepspace221@gmail.com>
2018-02-25 07:17:30 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -290,41 +290,6 @@ function getServerMsg(){
        return  getExpandableEmbed(title, "", color, thumb, description, fields, "", "");   
 }
 
-function crossReferenceLangs(arrNative, arrFluent, arrLearning){
-      var arrExceptions = ["Welsh", "Gaelic"];
-      
-      var arrOutput = arrNative;
-      for (var i = 0; i < arrNative.length; i++){
-            if (!isValueInArr(arrLearning, arrNative[i]))
-                 arrOutput[i] = arrOutput[i] + createEmptyStr(20 - arrOutput[i].length) + "N";
-            else if (!isValueInArr(arrFluent, arrNative[i]))
-                     arrOutput[i] = arrOutput[i] + createEmptyStr(20 - arrOutput[i].length) + "N/L";
-      } 
-      for (var i = 0; i < arrLearning.length; i++){
-            if (isValueInArr(arrExceptions, arrLearning[i])){
-                  if (arrLearning[i] == "Welsh" || arrLearning[i] == "Gaelic")
-                         arrOutput.push(arrLearning[i] + createEmptyStr(20 - arrLearning[i].length) + "<Celtic>");         
-            }     
-            else if (!isValueInArr(arrOutput, arrLearning[i]))
-               arrOutput.push(arrLearning[i] + createEmptyStr(20 - arrLearning[i].length) + "L");
-      }
-      for (var i = 0; i < arrOutput.length; i++){
-          if (arrOutput[i] == "Esperanto")
-                arrOutput[i] = arrOutput[i] + createEmptyStr(20 - arrOutput[i].length) + "F/L";
-          else if (isValueInArr(arrFluent, arrOutput[i])){
-                arrOutput[i] = arrOutput[i] + createEmptyStr(20 - arrOutput[i].length) + "[All]";
-          }
-      }              
-
-      arrOutput = arrOutput.sort(function(a,b){
-            if (a > b) return 1;
-            if (a < b) return -1;
-            if (a == b) return 0;  
-      });
-      return arrOutput;
-}
-
-
 
 function getLanguagesEmbed(){
       use server_db; 
