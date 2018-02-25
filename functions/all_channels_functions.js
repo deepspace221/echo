3e2da254d51448f89342081499660aae29818c78
deepspace <deepspace221@gmail.com>
2018-02-25 10:17:58 +0200

    Update all_channels_functions.js

--- functions/all_channels_functions.js
+++ functions/all_channels_functions.js
@@ -1,48 +1,48 @@
-function getBaseLanguageStr(str){
-    if (/\s/.test(str))
-        str = str.slice(3);
-    return str;
-}
+// function getBaseLanguageStr(str){
+//     if (/\s/.test(str))
+//         str = str.slice(3);
+//     return str;
+// }
 
-function getCatResourcesChannel(category, name){
-       for (var i = 0; i < ServerChannels.length; i++){
-            if (ServerChannels[i].ParentID == category && ServerChannels[i].Name == name){
-                return ServerChannels[i].Name;
-       return "Not found.";
-}
+// function getCatResourcesChannel(category, name){
+//        for (var i = 0; i < ServerChannels.length; i++){
+//             if (ServerChannels[i].ParentID == category && ServerChannels[i].Name == name){
+//                 return ServerChannels[i].Name;
+//        return "Not found.";
+// }
 
-function getRoleRelatedChannel(role, type){
-      role = role.toLowerCase();
-      for (var i = 0; i < ServerChannels.length; i++){
-            if (type == "lang" && ServerChannels[i].Name == role){
-                var obj = {
-                    category: "",
-                    channels: []
-                };
-                obj.category = ServerChannels[i].ParentID;
-                obj.channels.push(ServerChannels[i].Name);
-                obj.channels.push(getCatResourcesChannel(obj.category, "resources"));
-                obj.channels.push(getCatResourcesChannel(obj.category, "music"));
-                return obj;
-            }
-            else if (type == "other" && ServerChannels[i].Name == role){
-                return  ServerChannels[i].Name;
-            }
-      }
-}
+// function getRoleRelatedChannel(role, type){
+//       role = role.toLowerCase();
+//       for (var i = 0; i < ServerChannels.length; i++){
+//             if (type == "lang" && ServerChannels[i].Name == role){
+//                 var obj = {
+//                     category: "",
+//                     channels: []
+//                 };
+//                 obj.category = ServerChannels[i].ParentID;
+//                 obj.channels.push(ServerChannels[i].Name);
+//                 obj.channels.push(getCatResourcesChannel(obj.category, "resources"));
+//                 obj.channels.push(getCatResourcesChannel(obj.category, "music"));
+//                 return obj;
+//             }
+//             else if (type == "other" && ServerChannels[i].Name == role){
+//                 return  ServerChannels[i].Name;
+//             }
+//       }
+// }
 
 function serverMap(){
-    var arrUserRoles = getArrSortedRolesByPosition();
-    roleSlices = JSON.parse(server_db["roleSlices"]); 
-    var arrLangs = roleSlices.lang.native.concat(roleSlices.lang.fluent).concat(roleSlices.lang.learning);
+//     var arrUserRoles = getArrSortedRolesByPosition();
+//     roleSlices = JSON.parse(server_db["roleSlices"]); 
+//     var arrLangs = roleSlices.lang.native.concat(roleSlices.lang.fluent).concat(roleSlices.lang.learning);
     
-    var channels = {
-        lang: [{}],
-        platforms: [],
-        hobbies: []
-    };
+//     var channels = {
+//         lang: [{}],
+//         platforms: [],
+//         hobbies: []
+//     };
     
-    var role;
+//     var role;
     
 //     for (var i = 0; i < arrUserRoles.length; i++){
 //         if (isValueInArr(arrLangs, arrUserRoles[i])){
