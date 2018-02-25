c26f7914721c4c8ed7fe26cc6a8a7901e9a0a598
deepspace <deepspace221@gmail.com>
2018-02-25 03:10:00 +0200

    Update init_db_values.js

--- functions/init_db_values.js
+++ functions/init_db_values.js
@@ -138,7 +138,7 @@ function storeServerRolesSlices(initValues){
         use server_db;
         var arrSortedServerRolesObj = [], newPos = 0;
   
-        if (initValues || server_db["ServerRolesSorted"] == undefined){
+        if (true || server_db["ServerRolesSorted"] == undefined){
                 arrSortedServerRolesObj = JSON.stringify(getSeverRolesArrSortedByPosition(0, 250, "obj", true));
                 server_db["ServerRolesSorted"] = arrSortedServerRolesObj;
                 dbg("storing `ServerRolesSorted`");
