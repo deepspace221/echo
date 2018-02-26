2ed24030c6f50ec2bd9a40de2b599b52bac7f5cc
deepspace <deepspace221@gmail.com>
2018-02-26 06:59:59 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -410,8 +410,8 @@ function getStaffEmbed(){
 dbg(emb.fields);
      emb.fields[0].name = "ccc";
      emb.fields[0].value = "ddd";
-     emb.fields[1].name = "aaa";
-     emb.fields[1].value = "dde";
+//      emb.fields[1].name = "aaa";
+//      emb.fields[1].value = "dde";
 //      emb.fields[0].value = createArrOutputCommaSeprated(staff.owners.users);
 //      emb.fields[1].name = "Admins";
 //      emb.fields[1].value = createArrOutputCommaSeprated(staff.admins.users);     
