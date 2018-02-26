5a31680fa33dbab9a38a24edbd83b2333ae50c89
deepspace <deepspace221@gmail.com>
2018-02-26 10:19:00 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -406,7 +406,7 @@ function getStaffEmbed(){
      emb.title = "☁☁ Kingdom of Valhalla ☁☁";
      emb.description = "The imperial guards <:battle_cry:402261899221139466>";
      emb.fields = getFieldsObj(9, false);
-     emb.thumb.url = "https://www.duolingo.com/images/illustrations/owl-happy@2x.png";
+     emb.thumbnail.url = "https://www.duolingo.com/images/illustrations/owl-happy@2x.png";
 //      emb.image.url = "http://valhallayork.com/wp-content/uploads/2017/08/Web.png";
       
      staff.botDev.users[0] += " ☃";
