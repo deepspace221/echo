a4a137f1904a40160abce474203ba738815e38ef
deepspace <deepspace221@gmail.com>
2018-02-26 18:04:18 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -508,7 +508,7 @@ function getPatronsEmbed(){
                   var arr1 = arr.splice(0, arr.length/2);
                   var arr2 = arr;
                   emb.fields[i].name = "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■";
-                  emb.fields[i].value = "```css\n      [Join a new and exciting thing! We need you!]      ```";
+                  emb.fields[i].value = "```css\n  [Join a new and exciting thing! We need you!]  ```";
                   emb.fields[i].inline = false;
                   emb.fields[i+1].name = "Open Positions<:blank:352901517004636163>";
                   emb.fields[i+1].value = "```css\n" + createArrOutputNewLinesSeprated(arr1)+ "```";
@@ -529,9 +529,5 @@ function getPatronsEmbed(){
       
 }
 
-function getServerMap(){
-      
-      
-      
-}
+
 
