84f3c4d76fcb2d03207e01a5bf8cf339cf9601a7
deepspace <deepspace221@gmail.com>
2018-02-26 17:50:03 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -507,7 +507,7 @@ function getPatronsEmbed(){
                   }); 
                   var arr1 = arr.splice(0, arr.length/2);
                   var arr2 = arr;
-                  emb.fields[i].name = "<:blank:352901517004636163>";
+                  emb.fields[i].name = "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■";
                   emb.fields[i].value = "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■";
                   emb.fields[i].inline = false;
                   emb.fields[i+1].name = "Open Positions<:blank:352901517004636163>";
