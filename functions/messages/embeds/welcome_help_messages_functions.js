5c046329f1ad7b6b1cbceca2c915a70b6f57f5e4
deepspace <deepspace221@gmail.com>
2018-02-26 13:03:17 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -501,7 +501,7 @@ function getPatronsEmbed(){
                   break;     
            }
            emb.fields[i].name = patrons[i].role.replace(/patron/gi, "P.");
-           if (patrons[i+!].users == "" & i % 2 == 1){
+           if (patrons[i+1].users == "" && i % 2 == 1){
               emb.fields[i].value = (createArrOutputNewLinesSeprated(patrons[i].users)) ? createArrOutputNewLinesSeprated(patrons[i].users) : "NaN";   
               emb.fields[i].inline = false;      
            }
