60d69cb443fb55d8190d598522e6ed5ae1793366
deepspace <deepspace221@gmail.com>
2018-02-26 10:46:42 +0200

    Update welcome_help_messages_functions.js

--- functions/messages/embeds/welcome_help_messages_functions.js
+++ functions/messages/embeds/welcome_help_messages_functions.js
@@ -469,3 +469,22 @@ function getStaffEmbed(){
       
      return emb;       
 }
+function getPatronsEmbed(){
+      
+     use server_db; 
+     var roleSlices, patrons;
+     var emb = getEmbedObj();
+      
+     roleSlices = JSON.parse(server_db["roleSlices"]);  
+     patrons = roleSlices.patrons;
+      
+     dbg(patrons);
+      
+//      emb.title = "☁☁ Kingdom of Valhalla ☁☁";
+//      emb.description = "The imperial guards <:battle_cry:402261899221139466>";
+//      emb.fields = getFieldsObj(8, false);
+//      emb.thumbnail.url = "https://www.duolingo.com/images/illustrations/owl-happy@2x.png";
+// //      emb.image.url = "http://valhallayork.com/wp-content/uploads/2017/08/Web.png";
+      
+}
+
