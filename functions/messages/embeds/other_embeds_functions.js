897b7a7bb9b022e146be166a93683fcaa759618f
deepspace <deepspace221@gmail.com>
2018-03-01 21:18:13 +0200

    Update other_embeds_functions.js

--- functions/messages/embeds/other_embeds_functions.js
+++ functions/messages/embeds/other_embeds_functions.js
@@ -230,7 +230,7 @@ function getInRoleNextPreviousPage(type){
 	if (server_db.hasOwnProperty("inRole")){
 // 		dbg("in");
 		inRole = JSON.parse(server_db["inRole"]);
-		if ((type == "next" && inRole.page < inRole.pagesNum) || (type == "previous" && inRole.page > 0)){
+		if ((type == "next" && inRole.page < inRole.pagesNum-1) || (type == "previous" && inRole.page > 0)){
 			
 			if (type == "next" && (inRole.page + 1 <= inRole.pagesNum)){
 				inRole.page++;
@@ -240,7 +240,7 @@ function getInRoleNextPreviousPage(type){
 				inRole.page--;
 				emb = getInRolePage(inRole.emb, inRole.arrUsers, inRole.page);			
 			}
-			emb.footer.text = "Page " + inRole.page+1 + "/" + inRole.pagesNum;	
+			emb.footer.text = "Page " + (inRole.page + 1) + "/" + inRole.pagesNum;	
 			server_db["inRole"] = JSON.stringify(inRole); 
 
 			return getJSEmbedToArs(emb) + inRole.reacts;
