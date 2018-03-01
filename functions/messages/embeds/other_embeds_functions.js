b4ad9945d3103a1f33628cd318800a6f2e3032a8
deepspace <deepspace221@gmail.com>
2018-03-01 07:35:53 +0200

    Update other_embeds_functions.js

--- functions/messages/embeds/other_embeds_functions.js
+++ functions/messages/embeds/other_embeds_functions.js
@@ -196,8 +196,9 @@ function getInRoleEmbed(arrUsers, role){
      emb.title = "Users in the role - " + role;
      emb.description = "We have **" + arrUsers.length + "** users in the role **" + role + "**.";
      emb.thumbnail.url = "https://zartnerds.files.wordpress.com/2015/10/thumbnail.png";
-     emb.image.url = "https://zartnerds.files.wordpress.com/2015/10/thumbnail.png";
+     emb.image.url = "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678110-sign-info-128.png";
      emb.footer.text = "this is a footer";
+     emb.footer.text_url = "https://zartnerds.files.wordpress.com/2015/10/thumbnail.png";	
 	
      if (arrUsers.length <= 20){	
     	 emb.fields = getFieldsObj(1, true);
