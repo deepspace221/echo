35e2767a2649024fe7c379322957eb1bacbfa020
deepspace <deepspace221@gmail.com>
2018-03-01 05:41:03 +0200

    Update other_embeds_functions.js

--- functions/messages/embeds/other_embeds_functions.js
+++ functions/messages/embeds/other_embeds_functions.js
@@ -211,5 +211,5 @@ function getInRoleEmbed(arrUsers, role){
      if (arrUsers.length != 0){
 	reacts = "{reactbot::arrow_backward: :arrow_forward:}"    
      }
-     return getJSEmbedToArs(emb);
+     return getJSEmbedToArs(emb) + reacts;
 }
