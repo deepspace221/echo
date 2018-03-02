b6f8dd21c6f37ffce1d360e0dd0f8fc9f2facb44
deepspace <deepspace221@gmail.com>
2018-03-02 13:29:26 +0200

    Update bots_playground_whoami.js

--- js/bots-playground/bots_playground_whoami.js
+++ js/bots-playground/bots_playground_whoami.js
@@ -306,15 +306,4 @@ function getDisplayWhoami(){
                 }
                 return user;
         } 
-         function getMemberIndex() { //WORKING
-            var memberIndex = 0;
-
-            for (i = 0; i < ServerMembers.length; i++) {
-                if (ServerMembers[i].User.ID == UserID) {
-                    memberIndex = i;
-                    break;
-                }
-            }
-            return memberIndex;
-        }
 }
