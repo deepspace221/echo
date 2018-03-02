584dec338d4abee1470e81e8f2386d2e53977197
deepspace <deepspace221@gmail.com>
2018-03-02 13:29:11 +0200

    Update ServerMembers_functions.js

--- functions/ServerMembers_functions.js
+++ functions/ServerMembers_functions.js
@@ -1,14 +1,14 @@
-// function getMemberIndex() { //WORKING
-//     var memberIndex = 0;
+function getMemberIndex() { //WORKING
+    var memberIndex = 0;
 
-//     for (i = 0; i < ServerMembers.length; i++) {
-//         if (ServerMembers[i].User.ID == UserID) {
-//             memberIndex = i;
-//             break;
-//         }
-//     }
-//     return memberIndex;
-// }
+    for (i = 0; i < ServerMembers.length; i++) {
+        if (ServerMembers[i].User.ID == UserID) {
+            memberIndex = i;
+            break;
+        }
+    }
+    return memberIndex;
+}
 
 function getUserRolesArr() {
     var roles = [];
